5b00e1b1f9d41f68952bc3decfc545427f726236
Annie Chiang <devbear22@gmail.com>
2017-12-14 18:55:03 -0800

    initial commit - successful test

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { configure, mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+import GuessForm from './guess-form';
+
+describe('<GuessForm />', () => {
+  it('renders without crashing', () => {
+    shallow(<GuessForm />);
+  });
+
+  it('should fire onMakeGuess callback when form is submitted', () => {
+    const callback = jest.fn();
+    const wrapper = mount(<GuessForm onMakeGuess={callback} />);
+    const value = 10;
+    wrapper.find('input[type="number"]').instance().value = value;
+    wrapper.simulate('submit');
+    expect(callback).toHaveBeenCalledWith(value.toString());
+  });
+
+  it('should reset input when form is submitted', () => {
+    const wrapper = mount(<GuessForm />);
+    const input = wrapper.find('input[type="number"]');
+    input.instance().value = 10;
+    wrapper.simulate('submit');
+    expect(input.instance().value).toEqual('');
+  });
+});
